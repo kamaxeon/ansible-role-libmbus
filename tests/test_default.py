6121bc922ab5006990c5a6b6f9d59e545eae080d
Israel Santana <isantana@inerza.com>
2017-03-09 13:41:49 +0000

    Check only base packages

--- tests/test_default.py
+++ tests/test_default.py
@@ -5,47 +5,56 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-# Binary files
+# Libmbus Base Package
 @pytest.mark.parametrize('name', [
-    ('mbus-serial-request-data'),
-    ('mbus-serial-request-data-multi-reply'),
-    ('mbus-serial-scan'),
-    ('mbus-serial-scan-secondary'),
-    ('mbus-serial-select-secondary'),
-    ('mbus-serial-set-address'),
-    ('mbus-serial-switch-baudrate'),
-    ('mbus-tcp-application-reset'),
-    ('mbus-tcp-raw-send'),
-    ('mbus-tcp-request-data'),
-    ('mbus-tcp-request-data-multi-reply'),
-    ('mbus-tcp-scan'),
-    ('mbus-tcp-scan-secondary'),
-    ('mbus-tcp-select-secondary'),
+    ('git'),
+    ('libtool'),
+    ('automake'),
+    ('make'),
 ])
-def test_binary_files(File, name):
-    assert File(name).exists
-    assert File(name).is_file
-    assert File(name).user == 'root'
-    assert File(name).mode == 0o644
+def test_base_packes(Package, name):
+    assert Package(name).is_installed
 
 
-# Binary files
-@pytest.mark.parametrize('name', [
-    ('mbus-serial-request-data'),
-    ('mbus-serial-request-data-multi-reply'),
-    ('mbus-serial-scan'),
-    ('mbus-serial-scan-secondary'),
-    ('mbus-serial-select-secondary'),
-    ('mbus-serial-set-address'),
-    ('mbus-serial-switch-baudrate'),
-    ('mbus-tcp-application-reset'),
-    ('mbus-tcp-raw-send'),
-    ('mbus-tcp-request-data'),
-    ('mbus-tcp-request-data-multi-reply'),
-    ('mbus-tcp-scan'),
-    ('mbus-tcp-scan-secondary'),
-    ('mbus-tcp-select-secondary'),
-])
-def test_run_binary(Command, name):
-    cmd = (Command(name))
-    assert 'mbus-serial' in cmd.stdout
+# # Binary files
+# @pytest.mark.parametrize('name', [
+#     ('mbus-serial-request-data'),
+#     ('mbus-serial-request-data-multi-reply'),
+#     ('mbus-serial-scan'),
+#     ('mbus-serial-scan-secondary'),
+#     ('mbus-serial-select-secondary'),
+#     ('mbus-serial-set-address'),
+#     ('mbus-serial-switch-baudrate'),
+#     ('mbus-tcp-application-reset'),
+#     ('mbus-tcp-raw-send'),
+#     ('mbus-tcp-request-data'),
+#     ('mbus-tcp-request-data-multi-reply'),
+#     ('mbus-tcp-scan'),
+#     ('mbus-tcp-scan-secondary'),
+#     ('mbus-tcp-select-secondary'),
+# ])
+# def test_binary_files(File, name):
+#     assert File(name).exists
+#     assert File(name).is_file
+#     assert File(name).user == 'root'
+#     assert File(name).mode == 0o644
+# # Binary files
+# @pytest.mark.parametrize('name', [
+#     ('mbus-serial-request-data'),
+#     ('mbus-serial-request-data-multi-reply'),
+#     ('mbus-serial-scan'),
+#     ('mbus-serial-scan-secondary'),
+#     ('mbus-serial-select-secondary'),
+#     ('mbus-serial-set-address'),
+#     ('mbus-serial-switch-baudrate'),
+#     ('mbus-tcp-application-reset'),
+#     ('mbus-tcp-raw-send'),
+#     ('mbus-tcp-request-data'),
+#     ('mbus-tcp-request-data-multi-reply'),
+#     ('mbus-tcp-scan'),
+#     ('mbus-tcp-scan-secondary'),
+#     ('mbus-tcp-select-secondary'),
+# ])
+# def test_run_binary(Command, name):
+#     cmd = (Command(name))
+#     assert 'mbus-serial' in cmd.stdout
