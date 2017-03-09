342924d82d589d4d03e6635df8eb54aa8eff4ef2
Israel Santana <isantana@inerza.com>
2017-03-09 16:52:57 +0000

    Fix binary files and permissions

--- tests/test_default.py
+++ tests/test_default.py
@@ -24,26 +24,25 @@ def test_repo_dest(File):
 
 # Binary files
 @pytest.mark.parametrize('name', [
-    ('mbus-serial-request-data'),
-    ('mbus-serial-request-data-multi-reply'),
-    ('mbus-serial-scan'),
-    ('mbus-serial-scan-secondary'),
-    ('mbus-serial-select-secondary'),
-    ('mbus-serial-set-address'),
-    ('mbus-serial-switch-baudrate'),
-    ('mbus-tcp-application-reset'),
-    ('mbus-tcp-raw-send'),
-    ('mbus-tcp-request-data'),
-    ('mbus-tcp-request-data-multi-reply'),
-    ('mbus-tcp-scan'),
-    ('mbus-tcp-scan-secondary'),
-    ('mbus-tcp-select-secondary'),
+    ('/usr/local/bin/mbus-serial-request-data'),
+    ('/usr/local/bin/mbus-serial-request-data-multi-reply'),
+    ('/usr/local/bin/mbus-serial-scan'),
+    ('/usr/local/bin/mbus-serial-scan-secondary'),
+    ('/usr/local/bin/mbus-serial-select-secondary'),
+    ('/usr/local/bin/mbus-serial-switch-baudrate'),
+    ('/usr/local/bin/mbus-tcp-application-reset'),
+    ('/usr/local/bin/mbus-tcp-raw-send'),
+    ('/usr/local/bin/mbus-tcp-request-data'),
+    ('/usr/local/bin/mbus-tcp-request-data-multi-reply'),
+    ('/usr/local/bin/mbus-tcp-scan'),
+    ('/usr/local/bin/mbus-tcp-scan-secondary'),
+    ('/usr/local/bin/mbus-tcp-select-secondary'),
 ])
 def test_binary_files(File, name):
     assert File(name).exists
     assert File(name).is_file
     assert File(name).user == 'root'
-    assert File(name).mode == 0o644
+    assert File(name).mode == 0o755
 # # Binary files
 # @pytest.mark.parametrize('name', [
 #     ('mbus-serial-request-data'),
