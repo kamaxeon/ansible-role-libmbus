cdfc626fcc1758be909f34d91d96f8731158d5e7
Israel Santana <isra@miscorreos.org>
2017-03-14 01:27:30 +0000

    Fix arguments

--- run_travis.sh
+++ run_travis.sh
@@ -4,8 +4,8 @@ git clone https://github.com/kamaxeon/ansible-role-libmbus.git libmbus
 cd libmbus
 sed -i '/ansible==/d' requirements.txt
 pip install -r requirements.txt ansible==$1
-sed -i "s/DOCKER_IMAGE_VERSION/$2/g" molecule_tmp.yml
-sed -i "s/DOCKER_IMAGE/$1/g" molecule_tmp.yml
-sed -i "s/DOCKER_NAME/$1_$2/g" molecule_tmp.yml
+sed -i "s/DOCKER_IMAGE_VERSION/$3/g" molecule_tmp.yml
+sed -i "s/DOCKER_IMAGE/$2/g" molecule_tmp.yml
+sed -i "s/DOCKER_NAME/$2_$3/g" molecule_tmp.yml
 cp -a molecule_tmp.yml molecule.yml
 molecule test
