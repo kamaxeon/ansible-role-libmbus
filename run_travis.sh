5519742b86a372cdd33bbe11d7ad49e606fc83e9
Israel Santana <isra@miscorreos.org>
2017-03-14 01:23:54 +0000

    Updata travis script

--- run_travis.sh
+++ run_travis.sh
@@ -3,6 +3,9 @@ cd /tmp/
 git clone https://github.com/kamaxeon/ansible-role-libmbus.git libmbus
 cd libmbus
 sed -i '/ansible==/d' requirements.txt
-pip install -r requirements.txt
-pip install ansible==$1
+pip install -r requirements.txt ansible==$1
+sed -i "s/DOCKER_IMAGE_VERSION/$2/g" molecule_tmp.yml
+sed -i "s/DOCKER_IMAGE/$1/g" molecule_tmp.yml
+sed -i "s/DOCKER_NAME/$1_$2/g" molecule_tmp.yml
+cp -a molecule_tmp.yml molecule.yml
 molecule test
