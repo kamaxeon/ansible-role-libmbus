6f1b100302a60417d58f5c3550408098acd3c2ff
Israel Santana <isra@miscorreos.org>
2017-03-14 02:00:11 +0000

    Add requirements.txt

--- run_travis.sh
+++ run_travis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip install ansible==$ANSIBLE_VERSION
+pip install -r requirements.txt ansible==$ANSIBLE_VERSION
 sed -i "s/DOCKER_IMAGE_VERSION/$DOCKER_IMAGE_VERSION/g" molecule_tmp.yml
 sed -i "s/DOCKER_IMAGE/$DOCKER_IMAGE/g" molecule_tmp.yml
 sed -i "s/DOCKER_NAME/$DOCKER_IMAGE_$DOCKER_IMAGE_VERSION/g" molecule_tmp.yml
