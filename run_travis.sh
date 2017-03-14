b6b1a2d0f2b689246102eefd20942bdfca60ddd0
Israel Santana <isra@miscorreos.org>
2017-03-14 01:51:56 +0000

    Change travis strategy

--- run_travis.sh
+++ run_travis.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-cd /tmp/
-git clone https://github.com/kamaxeon/ansible-role-libmbus.git libmbus
-cd libmbus
-sed -i '/ansible==/d' requirements.txt
-pip install -r requirements.txt ansible==$1
-sed -i "s/DOCKER_IMAGE_VERSION/$3/g" molecule_tmp.yml
-sed -i "s/DOCKER_IMAGE/$2/g" molecule_tmp.yml
-sed -i "s/DOCKER_NAME/$2_$3/g" molecule_tmp.yml
+pip install ansible==$ANSIBLE_VERSION
+sed -i "s/DOCKER_IMAGE_VERSION/$DOCKER_IMAGE_VERSION/g" molecule_tmp.yml
+sed -i "s/DOCKER_IMAGE/$DOCKER_IMAGE/g" molecule_tmp.yml
+sed -i "s/DOCKER_NAME/$DOCKER_IMAGE_$DOCKER_IMAGE_VERSION/g" molecule_tmp.yml
 cp -a molecule_tmp.yml molecule.yml
 molecule test
