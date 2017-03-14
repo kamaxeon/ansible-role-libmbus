ebc1e9c7d8a77a3f54432cf1121382f9216ced4d
Israel Santana <isra@miscorreos.org>
2017-03-14 02:28:45 +0000

    Remove inncesary files

--- run_travis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pip install -r requirements.txt ansible==$ANSIBLE_VERSION
-sed -i "s/DOCKER_IMAGE_VERSION/$DOCKER_IMAGE_VERSION/g" molecule_tmp.yml
-sed -i "s/DOCKER_IMAGE/$DOCKER_IMAGE/g" molecule_tmp.yml
-sed -i "s/DOCKER_NAME/$DOCKER_IMAGE_$DOCKER_IMAGE_VERSION/g" molecule_tmp.yml
-cp -a molecule_tmp.yml molecule.yml
-molecule test
